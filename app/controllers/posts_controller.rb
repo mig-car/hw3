52b63185a949e0dfce54c3fdf8e167b31b0e8d9d
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-02-20 22:57:05 +0000

    Fixed bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,16 @@ class PostsController < ApplicationController
     end 
 
     def new
-        @post = Post.new
+        @post = Post.new(posted_on: Time.now)
         @place = Place.find_by({ "id" => params["place_id"] })
-        #@post["place_id"] = @place["id"]
     end
 
     def create
         @post = Post.new
-
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["place_id"]
         @post.save
         redirect_to "/places/#{@post["place_id"]}"
 
