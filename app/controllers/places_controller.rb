8620d0d5f08b7e6a75a82e2c58b3c9620a39646c
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-02-20 20:58:46 +0000

    Fixed new post form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,6 +6,7 @@ class PlacesController < ApplicationController
 
     def show
         @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.all
     end
 
     def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+    def index
+        
+    end
+
+    def show
+        @post = Post.find_by({ "id" => params["id"] })
+    end 
+
+    def new
+        @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        #@post["place_id"] = @place["id"]
+    end
+
+    def create
+        @post = Post.new
+
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+
+    end
+
 end
